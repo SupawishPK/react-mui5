4c9a6f89e959a91aa967d4af37a43435c86dcf50
Supawish kaewjing <supawish.kj@gmail.com>
2022-01-11 21:06:33 +0700

    feat: complete tour card

--- src/components/compose/TourCard/TourCard.tsx
+++ src/components/compose/TourCard/TourCard.tsx
@@ -1,10 +1,62 @@
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>Tour card</Paper>
+      <Paper elevation={3}>
+        <img
+          src="http://pix4.agoda.net/hotelimages/301315/-1/f37a109db517a20aacc78177b0c9687e.jpg"
+          alt=""
+          className="img"
+        />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            Immerse into the Falls
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+            marginTop={3}
+          >
+            <Rating
+              name="read-only"
+              value={4.5}
+              readOnly
+              precision={0.5}
+              size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="caption" component="p" marginLeft={1.5}>
+              (655 review)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" component="h3">
+              From C $147
+            </Typography>
+          </Box>
+        </Box>
+      </Paper>
     </Grid>
   );
 };
