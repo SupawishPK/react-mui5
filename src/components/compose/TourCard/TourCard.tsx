00d057cb465a68c415d020bac65205bfa1198838
Supawish kaewjing <supawish.kj@gmail.com>
2022-01-11 21:32:52 +0700

    feat: create theme

--- src/components/compose/TourCard/TourCard.tsx
+++ src/components/compose/TourCard/TourCard.tsx
@@ -4,59 +4,87 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { AccessTime } from "@mui/icons-material";
 import Rating from "@mui/material/Rating";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "caption",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="http://pix4.agoda.net/hotelimages/301315/-1/f37a109db517a20aacc78177b0c9687e.jpg"
-          alt=""
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src="http://pix4.agoda.net/hotelimages/301315/-1/f37a109db517a20aacc78177b0c9687e.jpg"
+            alt=""
+            className="img"
+          />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Immerse into the Falls
             </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              marginTop={3}
+            >
+              <Rating
+                name="read-only"
+                value={4.5}
+                readOnly
+                precision={0.5}
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="caption" component="p" marginLeft={1.5}>
+                (655 review)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3">
+                From C $147
+              </Typography>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-            marginTop={3}
-          >
-            <Rating
-              name="read-only"
-              value={4.5}
-              readOnly
-              precision={0.5}
-              size="small"
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="caption" component="p" marginLeft={1.5}>
-              (655 review)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3">
-              From C $147
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
